6608e7f836431600a75daae1dc9323c539398fe8
kabirz <kabir.zhong@qq.com>
2019-05-18 02:35:28 +0800

    add progress bar

--- openmv/entry/flash.py
+++ openmv/entry/flash.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import openmv
 import argparse
-
+from progressbar import ProgressBar
 
 def main():
     MAX_BUF_SIZE = 64
@@ -27,21 +27,20 @@ def main():
     # erase flash
     sect = boot.bootloader_flash()
     print('start erase flash')
-    for i in range(sect[1], sect[2]+1):
+    prog = ProgressBar()
+    for i in prog(range(sect[1], sect[2]+1)):
         boot.flash_erase(i)
-        hper = int(float((i-sect[1])/(sect[2]-sect[1]))*100)
-        print('\rflash erase:[{}{}] {}%'.format('=' * (hper//4), ' ' * (25-hper//4), hper), end='')
+     
     data = args.file.read()
 
     # write flash
     l, r = divmod(len(data), MAX_BUF_SIZE-4)
-    print('\nstart download image')
-    for i in range(l):
+    print('start download image')
+    prog = ProgressBar()
+    for i in prog(range(l)):
         boot.flash_write(data[(MAX_BUF_SIZE-4)*i: (MAX_BUF_SIZE-4)*(i+1)])
-        hper = int(float(i)/l*100)
-        print('\rflash write:[{}{}] {}%'.format('=' * (hper//4), ' ' * (25-hper//4), hper), end='')
+
     boot.flash_write(data[(MAX_BUF_SIZE-4)*l:])
-    print('\rflash write:[{}{}] {}%'.format('=' * 25, ' ' * (25-25), 100))
     # jump to openmv
     boot.bootloader_reset()
     print('flash success')
