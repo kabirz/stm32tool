bcb676109c14f6e912b359fa3f102b32a41242db
kabirz <kabir.zhong@qq.com>
2019-05-18 02:38:15 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup
 setup(
     name="stmtool",
     description="Python openmv tool for stm32",
-    version='0.0.4',
+    version='0.0.5',
     author="Zhong huiping",
     author_email="jxwazxzhp@126.com",
     url="https://github.com/kabirz/openmv",
@@ -26,6 +26,7 @@ setup(
         'Pillow',
         'pygame',
         'numpy',
+        'progressbar',
     ],
     classifiers=[
         'Intended Audience :: Developers',
